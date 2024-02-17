2a7bec6597672f7d44f0c404870c65f06d4aff99
Alex Flores Z <lexzum10@gmail.com>
2024-02-17 12:13:32 -0500

    fix add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server'
-});
+  output: 'server',
+  adapter: vercelServerless(),
+})
